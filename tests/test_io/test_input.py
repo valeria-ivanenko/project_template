8d4f339cb5bacbc3d22908395d7bf3acefbf4020
Valeriia Ivanenko <valeria.ivanenko@icloud.com>
2024-03-17 23:11:39 +0200

    Some changes to the input tests logic

--- tests/test_io/test_input.py
+++ tests/test_io/test_input.py
@@ -3,14 +3,14 @@ from app.io.input import read_file_builtin, read_file_pandas
 
 class TestReadFile(unittest.TestCase):
     def test_read_file_builtin(self):
-        file_path = "data/sample.txt"
+        file_path = "data/input/sample.txt"
         expected_text = "Hello World! This .txt was created by Valeriia Ivanenko"
         actual_text = read_file_builtin(file_path)
         self.assertEqual(actual_text.strip(), expected_text)
 
     def test_read_file_pandas(self):
-        csv_file_path = "data/sample.csv"
-        expected_text = "Hello World! This .csv was created by Valeriia Ivanenko"
+        csv_file_path = "data/input/sample.csv"
+        expected_text = "Empty DataFrame\nColumns: [Hello] \nIndex: []"
         actual_text = read_file_pandas(csv_file_path)
         self.assertEqual(actual_text.strip(), expected_text)
 
